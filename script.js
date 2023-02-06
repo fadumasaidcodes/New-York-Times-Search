64b60adad6b2ffad5c6da70b3d01c9ea8c9bf36e
fadumasaidibrahim <fadumasaid2022@gmail.com>
2023-02-06 09:50:54 +0000

    queryURL is the url we'll use to query the API

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
  * pulls information from the form and build the query URL
  * @returns {string} URL for NYT API based on form inputs
  */
+function buildQueryURL() {
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  
+    
+}
